66b063df8a3478bef07afc8e0a0232288bdbf18e
“liziyu403” <liziyu0104@gmail.com>
2023-11-23 23:31:56 +0100

    update display

--- multitasking_training_practical_work/T1/displayManager.c
+++ multitasking_training_practical_work/T1/displayManager.c
@@ -19,11 +19,13 @@ static void *display( void *parameters );
 
 
 void displayManagerInit(void){
-	//TODO
+	//TODO - check
+	pthread_create(&displayThread, NULL, display, NULL); // 创建线程，调用 display() 接口
 }
 
 void displayManagerJoin(void){
-	//TODO	
+	//TODO - check
+	pthread_join(displayThread, NULL); // 线程返回
 } 
 
 static void *display( void *parameters )
@@ -32,8 +34,13 @@ static void *display( void *parameters )
 	unsigned int diffCount = 0;
 	while(diffCount < DISPLAY_LOOP_LIMIT){
 		sleep(DISPLAY_SLEEP_TIME);
-		//TODO
+		//TODO - check
+		msg = getCurrentSum(); // 调用 iMessagerAdder 接口
+		messageDisplay(&msg); // 调用 iDisplay 接口
+		diffCount = ((getProducedCount() - getConsumedCount()) <= 0) & DISPLAY_LOOP_LIMIT; // 防止消费大于生产产生错误，小于0是为了防止越界问题
 	}
 	printf("[displayManager] %d termination\n", gettid());
-   //TODO
+	//TODO - check
+	pthread_exit(NULL);
+	printf("Display Thread ended\n");
 }
