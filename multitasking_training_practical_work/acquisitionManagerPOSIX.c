a8c251d76e658fc4482900c4223c4a326302622e
“liziyu403” <liziyu0104@gmail.com>
2023-11-24 23:45:11 +0100

    partial

--- multitasking_training_practical_work/acquisitionManagerPOSIX.c
+++ multitasking_training_practical_work/acquisitionManagerPOSIX.c
@@ -1,13 +1,10 @@
 #define BUFFER_SIZE 4
 #define ERROR_INIT 1
 #define ERROR_SUCCESS 0
-#define SEMAPHORE_INITIAL_VALUE 0
+#define SEMAPHORE_INITIAL_VALUE 1
 #define SEM_NAME1 "/preambule_sem1"
 #define SEM_NAME2 "/preambule_sem2"
-#define SEM_NAME3 "/preambule_sem3"
-#define SEM_NAME4 "/preambule_sem4"
-#define SEM_NAME5 "/preambule_sem5"
-#define SEM_NAME6 "/preambule_sem6"
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,15 +40,13 @@ static void *produce(void *params);
 */
 //TODO
 // Mutex for produce counting operation
-pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER; // produceCount++; 
-pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER; // p = produceCount;
+// atomic write mutex
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER; 
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;  
+// atomic read mutex
+pthread_mutex_t m3 = PTHREAD_MUTEX_INITIALIZER;  
+pthread_mutex_t m4 = PTHREAD_MUTEX_INITIALIZER;  
 
-// atomic write semaphre
-sem_t *semaphore1; 
-sem_t *semaphore2; 
-// atomic read semaphre
-sem_t *semaphore3; 
-sem_t *semaphore4; 
 // global semaphre
 sem_t *semaphore_Libre; // Sem Libre
 sem_t *semaphore_Occ; // Sem Occupe
@@ -78,18 +73,8 @@ static unsigned int createSynchronizationObjects(void)
     	tabIndiceLib[i]=i;
     }
 	// initialization of semaphore
-	sem_unlink(SEM_NAME1);
-    sem_unlink(SEM_NAME2);
-    sem_unlink(SEM_NAME3);
-    sem_unlink(SEM_NAME4);
-    sem_unlink(SEM_NAME5);
-    sem_unlink(SEM_NAME6);
-    semaphore1 = sem_open(SEM_NAME1, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
-    semaphore2 = sem_open(SEM_NAME2, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
-    semaphore3 = sem_open(SEM_NAME3, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
-    semaphore4 = sem_open(SEM_NAME4, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
     semaphore_Libre = sem_open(SEM_NAME5, O_CREAT, 0644, BUFFER_SIZE);
-    semaphore_Occ = sem_open(SEM_NAME6, O_CREAT, 0644, SEMAPHORE_INITIAL_VALUE);
+    semaphore_Occ = sem_open(SEM_NAME6, O_CREAT, 0644, 0);
     
     if (((semaphore1 == SEM_FAILED) || (semaphore2 == SEM_FAILED)) || ((semaphore3 == SEM_FAILED)) || ((semaphore4 == SEM_FAILED)) || ((semaphore_Libre == SEM_FAILED) || (semaphore_Occ == SEM_FAILED))) 
     {
@@ -104,20 +89,14 @@ static unsigned int createSynchronizationObjects(void)
 static void incrementProducedCount(void)
 {
 	//TODO
-	pthread_mutex_lock(&m1);
-		printf("Owns the mutex for produceCount++\n");
-		produceCount++; // Assigning the global variables with a lock to prevent other threads from modifying them.
-	pthread_mutex_unlock(&m1);
+	produceCount++; 
 }
 
 unsigned int getProducedCount(void)
 {
 	unsigned int p = 0;
 	//TODO
-	pthread_mutex_lock(&m2);
-		printf("Owns the mutex for produceCount get\n");
-		p = produceCount; // Assigning the operation with a lock prevents other threads from repeating the assignment.
-	pthread_mutex_unlock(&m2);
+	p = produceCount; 
 	return p;
 }
 
@@ -127,18 +106,17 @@ MSG_BLOCK getMessage(void){
 	MSG_BLOCK* X;
 	
 	sem_wait(semaphore_Occ); 
-	
-		sem_wait(semaphore3);
+		pthread_mutex_lock(&m3);
 			j_index = Table_IndicePlan[j_Plan];
 			j_Plan = ((j_Plan + 1)%BUFFER_SIZE);
-		sem_post(semaphore3);
+		pthread_mutex_unlock(&m3);
 						
 		X = dataBuffer[j_index]; // get address of MSG_BLOCK by the index j_index
 						
-		sem_wait(semaphore4);
+		pthread_mutex_lock(&m4);
 			Table_IndiceLib[j_Libre] = j_index;
 			j_Libre = ((j_Libre + 1)%BUFFER_SIZE);
-		sem_post(semaphore4);
+		pthread_mutex_unlock(&m4);
 
 	sem_post(semaphore_Libre); 
 	
@@ -177,10 +155,6 @@ void acquisitionManagerJoin(void)
 	}
 
 	//TODO
-	sem_destroy(semaphore1);
-	sem_destroy(semaphore2);
-	sem_destroy(semaphore3);
-	sem_destroy(semaphore4);
 	sem_destroy(semaphore_Occ);
 	sem_destroy(semaphore_Libre);
 	printf("[acquisitionManager]Semaphore cleaned\n");
@@ -199,18 +173,18 @@ void *produce(void* params)
 		sem_wait(semaphore_Libre); 
 			if(messageCheck(&temp)==1) // check pass
 			{		
-				sem_wait(semaphore1);
+				pthread_mutex_lock(&m1);
 					i_index = tabIndiceLib[i_Libre];
 					i_Libre = ((i_Libre + 1)%BUFFER_SIZE);
-				sem_post(semaphore1);
+				pthread_mutex_unlock(&m1);
 				getInput(i,&X);
 				dataBuffer[i_index] = &X; 
 				incrementProducedCount(); // produce ++
 				
-				sem_wait(semaphore2);
+				pthread_mutex_lock(&m2);
 					Table_IndicePlan[i_Plan]= i_index;
 					i_Plan = ((i_Plan + 1)%BUFFER_SIZE);
-				sem_post(semaphore2);
+				pthread_mutex_unlock(&m2);
 			}
         sem_post(semaphore_Occ); 	
 	}
